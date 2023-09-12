a235dc978f5a442326c08c97ea3bf5af867bb37d
Tyler Mifflin <you@example.com>
2023-09-11 21:02:45 -0600

    added mood options to the form

--- src/components/MoodForm.js
+++ src/components/MoodForm.js
@@ -10,3 +10,16 @@ const MoodForm = () => {
         localStorage.setItem('mood', mood);
         localStorage.setItem('thoughts', thoughts);
     };
+
+    return (
+        <form onSubmit = {handleSubmit}>
+            <label>
+                Mood:
+                <select value={mood} onChange={(e) => setMood(e.target.value)}>
+                    <option value="happy">Happy</option>
+                    <option value="sad">Sad</option>
+                    <option value="angry">Angry</option>
+                    <option value="anxious">Anxious</option>
+                    <option value="balanced">Balanced</option>
+                </select>
+            </label>
