b0a65b990b63fcc5c7e090a68f5ca8003411ee28
Tyler Mifflin <you@example.com>
2023-10-28 16:50:44 -0600

    working on MoodHistory to properly display the entire mood history

--- src/components/MoodHistory.js
+++ src/components/MoodHistory.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MoodEntry from './MoodEntry';
 
 const MoodHistory = () => {
-   const previousMoods = localStorage.getItem('moods');
-   const previousThoughts = localStorage.getItem('thoughts');
+   const [mood, setMood] = useState([]);
+    const [thoughts, setThoughts] = useState([]);
+
+    useEffect(() => {
+        const previousMoods = localStorage.getItem('mood')|| [];
+        const previousThoughts = localStorage.getItem('thoughts') || [];
+
+        setMood(previousMoods);
+        setThoughts(previousThoughts);
+    } , []);
+
 
 // display the previous moods and thoughts
 return (
     <div>
         <h2>Mood History</h2>
-        <MoodEntry mood={previousMoods} thoughts={previousThoughts} />
-    </div>
-);
+        {mood.len}
 }
 
 export default MoodHistory;
